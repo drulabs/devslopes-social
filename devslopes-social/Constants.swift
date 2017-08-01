2e04a8fa6b3329d23c7a7b1014308bcf30859bc6
drulabs <kaushal.devil009@gmail.com>
2017-08-01 19:19:36 +0530

    Sign in custom classes done

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Kaushal Dhruw on 01/08/17.
+//  Copyright © 2017 drulabs Inc. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 125.0/255.0
